1751adf9b89ce40fe6ca8f645ec0ed290cfc9487
Amrita977. <Amritarajbansh@gmail.com>
2025-06-19 00:04:00 +0530

    files

---
+++ resources/views/layouts.blade.php/footer.blade.php
@@ -0,0 +1,15 @@
+<footer id="footer" class="footer-home">
+  <div class="container">
+    <div class="footerMenu"><ul id="menu-footer-en" class="menu"><li id="menu-item-22411" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-22411"><a href="https://siddharthnagar.nic.in/website-policies/">Website Policies</a></li>
+<li id="menu-item-22412" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-22412"><a href="https://siddharthnagar.nic.in/help/">Help</a></li>
+<li id="menu-item-22413" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-22413"><a href="https://siddharthnagar.nic.in/contact-us/">Contact Us</a></li>
+<li id="menu-item-22414" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-22414"><a href="https://siddharthnagar.nic.in/feedback/">Feedback</a></li>
+</ul></div>    <div class="copyRights">
+      <div class="pd-bottom5 color-white ctnt-ownd-dis">Content Owned by District Administration</div>
+      <div class="copyRightsText">
+                <p> © District Siddharthnagar, Uttar Pradesh. Developed and hosted by <a href="http://www.nic.in/" target="_blank">National Informatics Centre </a>,<br><a href="http://meity.gov.in/" target="_blank"> Ministry of Electronics & Information Technology</a>, Government of India</p>
+					<p>Last Updated: <strong>Jun 13, 2025</strong></p>                  </div>
+        <div class="copyRightsLogos"> <a href="https://s3waas.gov.in/"><img src="images/S3WaaS.png" alt="Secure, Scalable and Sugamya Website as a Service opens a new window"></a> <a href="http://www.nic.in/"><img src="images/nicLogo.png" alt="National Informatics Centre opens a new window"></a> <a href="http://www.digitalindia.gov.in/"><img src="images/digitalIndia.png" alt="Digital India opens a new window"></a> <!-- <a href="#" class="stqc-logo"><img src="/common_utility/images/STQC-approved.png"  alt="STQC"></a> --> </div>
+      </div>
+    </div>
+  </footer>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('about-district', function () {
+    return view('components.about-district');
+});
+
